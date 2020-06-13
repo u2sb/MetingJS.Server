bff00dee189a2d2ac78c86c25fe21a2fa8c24d21
MonoLogue Chi <xxwhite.com@gmail.com>
2020-06-13 11:29:02 +0800

    修正fc函数错误问题

--- MetingJS.Server.Fc/FcRemoteEntrypoint.cs
+++ MetingJS.Server.Fc/FcRemoteEntrypoint.cs
@@ -1,21 +1,13 @@
 using Aliyun.Serverless.Core.Http;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace MetingJS.Server.Fc
 {
     public class FcRemoteEntrypoint : FcHttpEntrypoint
     {
-        protected override void Init(IWebHostBuilder webBuilder)
+        protected override void Init(IWebHostBuilder builder)
         {
-            webBuilder.ConfigureAppConfiguration((context, builder) =>
-            {
-                var env = context.HostingEnvironment;
-                builder.AddJsonFile("appsettings.json", true, true)
-                       .AddYamlFile("appsettings.yml", true, true)
-                       .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
-                       .AddJsonFile($"appsettings.{env.EnvironmentName}.yml", true);
-            }).UseStartup<Startup>();
+            builder.UseStartup<Startup>();
         }
     }
 }
--- MetingJS.Server.Fc/Program.cs
+++ MetingJS.Server.Fc/Program.cs
@@ -1,20 +1,9 @@
 using System;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
 
 namespace MetingJS.Server.Fc
 {
     public class Program
     {
-        // public static void Main(string[] args)
-        // {
-        //     CreateWebHostBuilder(args).Build().Run();
-        // }
-        //
-        // public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        //         WebHost.CreateDefaultBuilder(args)
-        //                .UseStartup<Startup>();
-
         public static void Main(string[] args)
         {
             Console.WriteLine("请不要这样启动");
